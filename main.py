fee390822edc81e248e9bee52a144ea860410073
wint3rmute <mateusz.baczek1998@gmail.com>
2019-02-27 15:14:27 +0100

    Requirements.txt & coloring the events according to the JSOS scheme

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import pprint
 def main():
     pp = pprint.PrettyPrinter(indent=4)
 
-    calendar_id = "ot43j3ekgcack0cu6bjf2sbvdo@group.calendar.google.com"
-    max_results = 1
+    calendar_id = "Your calendar id here (probably just your gmail adress)"
+    max_results = 500
     eventer = Eventer(calendar_id)
 
     list_of_events = eventer.get_upcoming_events(max_results)
@@ -20,11 +20,29 @@ def main():
     if not list_of_events:
         print('No upcoming events found.')
     for event in list_of_events:
-        # start = event['start'].get('dateTime', event['start'].get('date'))
-        # print(event["summary"])
-        pp.pprint(event)
-        eventer.set_color(event, Eventer.Color.Graphite)
-        eventer.update_event(event)
+        summary = event["summary"]
+
+        if summary.startswith('L '):
+            eventer.set_color(event, Eventer.Color.Lavender)
+            eventer.update_event(event)
+
+
+        if summary.startswith('P '):
+            eventer.set_color(event, Eventer.Color.Peacock)
+            eventer.update_event(event)
+
+
+        if summary.startswith('C '):
+            eventer.set_color(event, Eventer.Color.Banana)
+            eventer.update_event(event)
+
+
+        #pp.pprint(event)
+        
+        
+        
+        #eventer.set_color(event, Eventer.Color.Graphite)
+        #eventer.update_event(event)
 
 if __name__ == '__main__':
     main()
